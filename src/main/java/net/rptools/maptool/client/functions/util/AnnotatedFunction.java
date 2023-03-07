c220da0df9be0f6c4c5a4c6f37b7e1af18497b53
Kenneth VanderLinde <kwvanderlinde@gmail.com>
2023-03-07 09:57:25 -0800

    WIP prior to rebase

--- src/main/java/net/rptools/maptool/client/functions/util/AnnotatedFunction.java
+++ src/main/java/net/rptools/maptool/client/functions/util/AnnotatedFunction.java
@@ -1,5 +1,6 @@
 package net.rptools.maptool.client.functions.util;
 
+import net.rptools.maptool.util.FunctionUtil;
 import net.rptools.parser.Parser;
 import net.rptools.parser.ParserException;
 import net.rptools.parser.VariableResolver;
@@ -12,16 +13,29 @@ public class AnnotatedFunction extends AbstractFunction {
         Object invoke(List<Object> parameters) throws ParserException;
     }
 
-private final Callback callback;
+    private final int minParameters;
+    private final int maxParameters;
+    private final boolean isTrusted;
+    private final Callback callback;
 
-    public AnnotatedFunction(String name, int minParameters, int maxParameters, Callback callback) {
+    public AnnotatedFunction(String name, int minParameters, int maxParameters, boolean isTrusted, Callback callback) {
         // TODO Annotation for non-deterministic.
-        super(minParameters, maxParameters, true, name);
+        // We lie to the parser about the parameter counts because it does not give translated
+        // errors. Instead, we'll check it ourselves.
+        super(0, -1, true, name);
+        this.minParameters = minParameters;
+        this.maxParameters = maxParameters;
+        this.isTrusted = isTrusted;
         this.callback = callback;
     }
 
     @Override
     public Object childEvaluate(Parser parser, VariableResolver resolver, String functionName, List<Object> parameters) throws ParserException {
+        FunctionUtil.checkNumberParam(functionName, parameters, minParameters, maxParameters);
+        if (isTrusted) {
+            FunctionUtil.blockUntrustedMacro(functionName);
+        }
+
         return this.callback.invoke(parameters);
     }
 }
--- src/main/java/net/rptools/maptool/client/functions/util/MacroFunctionProcessor.java
+++ src/main/java/net/rptools/maptool/client/functions/util/MacroFunctionProcessor.java
@@ -10,9 +10,12 @@ import net.rptools.maptool.util.FunctionUtil;
 import net.rptools.parser.ParserException;
 import net.rptools.parser.function.Function;
 
+import javax.annotation.Nullable;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.function.Consumer;
 
 public class MacroFunctionProcessor {
@@ -22,112 +25,147 @@ public class MacroFunctionProcessor {
 
 	public void process(Consumer<Function> add) {
 		for (final var type : functionDefiningClasses) {
-			final Object instance;
-			try {
-				instance = type.getDeclaredConstructor().newInstance();
-			}
-			catch (InstantiationException | NoSuchMethodException | InvocationTargetException |
-			       IllegalAccessException e) {
-				return;
+			processType(type, add);
+		}
+	}
+
+	private <T> void processType(Class<T> type, Consumer<Function> add) {
+		final T instance;
+		try {
+			instance = type.getDeclaredConstructor().newInstance();
+		}
+		catch (InstantiationException | NoSuchMethodException | InvocationTargetException |
+		       IllegalAccessException e) {
+			return;
+		}
+
+		// We build a separate Function implementation for each method so that the parser can
+		// enforce parameter counts for us... in the future once we have those. Also in the
+		// future we will need to support multiple overloads for a given name, all part of the
+		// same Function presumably.
+
+		for (final var method : type.getDeclaredMethods()) {
+			final var annotation = method.getAnnotation(MacroFunction.class);
+			if (annotation == null) {
+				continue;
 			}
 
-			// We build a separate Function implementation for each method so that the parser can
-			// enforce parameter counts for us... in the future once we have those. Also in the
-			// future we will need to support multiple overloads for a given name, all part of the
-			// same Function presumably.
+			final var trusted = method.getAnnotation(Trusted.class);
+			final var transitional = method.getAnnotation(Transitional.class);
+			final var delimited = method.getAnnotation(Delimited.class);
 
-			for (final var method : type.getDeclaredMethods()) {
-				final var annotation = method.getAnnotation(MacroFunction.class);
-				if (annotation == null) {
+			Function function = null;
+			if (transitional != null) {
+				if (delimited == null) {
+					System.err.println("@Delimited cannot be used with @Transitional functions");
 					continue;
 				}
 
-				final var isTrusted = method.getAnnotation(Trusted.class) != null;
-				final var transitional = method.getAnnotation(Transitional.class);
-				final var delimited = method.getAnnotation(Delimited.class);
-
-				// TODO Allow the annotation to override the name.
-				final var name = method.getName();
-				final int minParameters;
-				final int maxParameters;
-				if (transitional != null) {
-					minParameters = transitional.minParameters();
-					maxParameters = transitional.maxParameters();
-				}
-				else {
-					minParameters = 0;
-					maxParameters = -1;
-				}
-
-				final java.util.function.BiFunction<Object, List<Object>, Object> resultTransformation;
-				if (delimited != null) {
-					// TODO Allow Iterable? Collection?
-					var isValid = false;
-					if (method.getGenericReturnType() instanceof ParameterizedType returnType) {
-						final var containerType = returnType.getRawType();
-						final var typeArgs = returnType.getActualTypeArguments();
-						isValid = List.class.equals(containerType)
-								&& typeArgs.length == 1 && String.class.equals(typeArgs[0]);
-					}
-
-					if (!isValid) {
-						System.err.printf("Found @Delimited method %s, but the return type is not a list. Skipping this function", name);
-						continue;
-					}
-					final var index = delimited.parameterIndex();
-					final var defaultDelim = delimited.ifMissing();
-
-					resultTransformation = (list, parameters) -> {
-						// TODO Can we enforce that this is List<String> somehow?
-						final var strings = (List<String>) list;
-						// TODO This lookup will need to be robust when we have overloads.
-						final var delim = (parameters.size() > index ? parameters.get(index) : defaultDelim).toString();
-						if ("json".equals(delim)) {
-							JsonArray jarr = new JsonArray();
-							strings.forEach(m -> jarr.add(new JsonPrimitive(m)));
-							return jarr;
-						}
-						else {
-							return StringFunctions.getInstance().join(strings, delim);
-						}
-					};
-				}
-				else {
-					resultTransformation = (a, p) -> a;
-				}
-
-				// Note that we lie to the parser about argument counts because it does not use
-				// translated error messages.
-				final var function = new AnnotatedFunction(name, 0, -1, parameters -> {
-					// Although the parser will check these for us, this also gives us the
-					// ability for translated messages, assuming we lie to the parser.
-					FunctionUtil.checkNumberParam(name, parameters, minParameters, maxParameters);
-					if (isTrusted && !MapTool.getParser().isMacroTrusted()) {
-						throw new ParserException(I18N.getText("macro.function.general.noPerm", name));
-					}
-
-					final Object result;
-					try {
-						result = method.invoke(instance, parameters);
-					}
-					catch (IllegalAccessException e) {
-						throw new ParserException(e);
-					}
-					catch (InvocationTargetException e) {
-						if (e.getTargetException() instanceof AnnotatedFunctionException afe) {
-							throw new ParserException(I18N.getText(afe.getKey(), name, afe.getParameters()));
-						}
-						if (e.getTargetException() instanceof ParserException pe) {
-							throw pe;
-						}
-						throw new ParserException(e);
-					}
-
-					return resultTransformation.apply(result, parameters);
-				});
-
-				add.accept(function);
+				function = processTransitional(instance, type, method, trusted, transitional);
 			}
+			else {
+				function = processTyped(instance, type, method, trusted);
+			}
+
+
+			add.accept(function);
 		}
+	}
+
+	private <T> @Nullable Function processTransitional(T instance, Class<T> type, Method method, Trusted trusted, Transitional transitional) {
+		// TODO Allow the annotation to override the name.
+		final var name = method.getName();
+		final int minParameters = transitional.minParameters();
+		final int maxParameters = transitional.maxParameters();
+		final var isTrusted = trusted != null;
+
+		// Note that we lie to the parser about argument counts because it does not use
+		// translated error messages.
+		return new AnnotatedFunction(name, minParameters, maxParameters, isTrusted, parameters -> {
+			try {
+				return method.invoke(instance, parameters);
+			}
+			catch (IllegalAccessException e) {
+				throw new ParserException(e);
+			}
+			catch (InvocationTargetException e) {
+				if (e.getTargetException() instanceof AnnotatedFunctionException afe) {
+					throw new ParserException(I18N.getText(afe.getKey(), name, afe.getParameters()));
+				}
+				if (e.getTargetException() instanceof ParserException pe) {
+					throw pe;
+				}
+				throw new ParserException(e);
+			}
+		});
+	}
+
+	private <T> @Nullable Function processTyped(T instance, Class<?> type, Method method, Trusted trusted) {
+		// TODO Support more than nullary functions.
+		// TODO Allow the annotation to override the name.
+		final var name = method.getName();
+		final int minParameters = 0;
+		final int maxParameters = 0;
+		final var isTrusted = trusted != null;
+
+		final var delimited = method.getAnnotation(Delimited.class);
+
+		BiFunction<Object, List<Object>, Object> resultTransformation;
+		if (delimited != null) {
+			// TODO Allow Iterable? Collection?
+			var isValid = false;
+			if (method.getGenericReturnType() instanceof ParameterizedType returnType) {
+				final var containerType = returnType.getRawType();
+				final var typeArgs = returnType.getActualTypeArguments();
+				isValid = List.class.equals(containerType)
+						&& typeArgs.length == 1 && String.class.equals(typeArgs[0]);
+			}
+
+			if (!isValid) {
+				System.err.printf("Found @Delimited method %s, but the return type is not a list. Skipping this function", name);
+				return null;
+			}
+			final var index = delimited.parameterIndex();
+			final var defaultDelim = delimited.ifMissing();
+
+			resultTransformation = (list, parameters) -> {
+				// TODO Can we enforce that this is List<String> somehow?
+				final var strings = (List<String>) list;
+				// TODO This lookup will need to be robust when we have overloads.
+				final var delim = (parameters.size() > index ? parameters.get(index) : defaultDelim).toString();
+				if ("json".equals(delim)) {
+					JsonArray jarr = new JsonArray();
+					strings.forEach(m -> jarr.add(new JsonPrimitive(m)));
+					return jarr;
+				}
+				else {
+					return StringFunctions.getInstance().join(strings, delim);
+				}
+			};
+		}
+		else {
+			resultTransformation = (result, parameters) -> result;
+		}
+
+		// Note that we lie to the parser about argument counts because it does not use
+		// translated error messages.
+		return new AnnotatedFunction(name, minParameters, maxParameters, isTrusted, parameters -> {
+			try {
+				return resultTransformation.apply(method.invoke(instance, parameters), parameters);
+			}
+			catch (IllegalAccessException e) {
+				throw new ParserException(e);
+			}
+			catch (InvocationTargetException e) {
+				if (e.getTargetException() instanceof AnnotatedFunctionException afe) {
+					throw new ParserException(I18N.getText(afe.getKey(), name, afe.getParameters()));
+				}
+				if (e.getTargetException() instanceof ParserException pe) {
+					throw pe;
+				}
+				throw new ParserException(e);
+			}
+		});
+
 	}
 }
