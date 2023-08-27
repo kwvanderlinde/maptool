d149b366f41a31a2b9188cbeb08581f9c7ec7794
Kenneth VanderLinde <kwvanderlinde@gmail.com>
2023-08-27 16:07:02 -0700

    Add getGlobbedResourceURIs with some basic logic to start at a reasonable root for walking

--- src/main/java/net/rptools/maptool/client/functions/ResourceFunction.java
+++ src/main/java/net/rptools/maptool/client/functions/ResourceFunction.java
@@ -14,9 +14,18 @@
  */
 package net.rptools.maptool.client.functions;
 
+import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 import net.rptools.maptool.language.I18N;
 import net.rptools.maptool.util.FunctionUtil;
 import net.rptools.parser.Parser;
@@ -24,14 +33,17 @@ import net.rptools.parser.ParserException;
 import net.rptools.parser.VariableResolver;
 import net.rptools.parser.function.AbstractFunction;
 import org.apache.commons.lang.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ResourceFunction extends AbstractFunction {
+  private static final Logger log = LogManager.getLogger(ResourceFunction.class);
 
   /** The singleton instance. */
   private static final ResourceFunction instance = new ResourceFunction();
 
   private ResourceFunction() {
-    super(0, -1, "getResourceURI");
+    super(0, -1, "getResourceURI", "getGlobbedResourceURIs");
   }
 
   /**
@@ -57,6 +69,80 @@ public class ResourceFunction extends AbstractFunction {
 
       return String.format(
           "resource://%s/%s", URLEncoder.encode(resourceLibraryName, StandardCharsets.UTF_8), path);
+    } else if (functionName.equalsIgnoreCase("getGlobbedResourceURIs")) {
+      FunctionUtil.checkNumberParam(functionName, parameters, 2, 2);
+
+      final String resourceLibraryName = parameters.get(0).toString();
+      final String glob = StringUtils.stripStart(parameters.get(1).toString(), "/");
+
+      // TODO Look this up in the asset panel somehow.
+      final var resourceLibraryRoot =
+          Paths.get(
+              "/home/kenneth/Nextcloud/RPGs/tools/MapTool/Resource Packs/Forgotten Adventures");
+
+      // TODO Define our own glob syntax. Constrain to *, ** and /, possibly ?. Validate that and
+      //  pull the top path out. Notably this will naturally prevent use of .. and should avoid
+      //  arbitrary directory traversal, though we should also be explicit in checking that.
+
+      var topPath = resourceLibraryRoot;
+      var globPath = Paths.get(glob);
+
+      final var matchedPaths = new ArrayList<Path>();
+
+      int i;
+      final var n = globPath.getNameCount();
+      final var pattern = Pattern.compile(".*?[*?\\[\\]{}].*");
+      // Only to n-1 because we don't want the top path to take the last component in case it is
+      // actually a direct references to a single file instead of a glob.
+      for (i = 0; i < n - 1; ++i) {
+        final var name = globPath.getName(i);
+        if (pattern.matcher(name.toString()).matches()) {
+          break;
+        }
+      }
+
+      if (i > 0) {
+        // Glob not present in the first element, only in some later element.
+        topPath = resourceLibraryRoot.resolve(globPath.subpath(0, i));
+        globPath = globPath.subpath(i, n);
+      }
+
+      final var rootDir = topPath;
+
+      try {
+        final var newGlob = globPath.toString();
+        final var pathMatcher = rootDir.getFileSystem().getPathMatcher("glob:" + newGlob);
+        Files.walkFileTree(
+            rootDir,
+            new SimpleFileVisitor<Path>() {
+              @Override
+              public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+                  throws IOException {
+                file = rootDir.relativize(file);
+                if (pathMatcher.matches(file)) {
+                  matchedPaths.add(file);
+                }
+                return FileVisitResult.CONTINUE;
+              }
+            });
+      } catch (Exception e) {
+        log.error(e);
+        throw new ParserException(e);
+      }
+
+      final var result = new ArrayList<String>();
+      for (final var path : matchedPaths) {
+        // TOOD Incorporate an ephemeral nonce.
+        final var uri =
+            String.format(
+                "resource://%s/%s",
+                URLEncoder.encode(resourceLibraryName, StandardCharsets.UTF_8), path);
+        result.add(uri);
+      }
+
+      return FunctionUtil.delimitedResult("json", result);
+
+      // TOOD Incorporate an ephemeral nonce.
     }
     throw new ParserException(I18N.getText("macro.function.general.unknownFunction", functionName));
   }
