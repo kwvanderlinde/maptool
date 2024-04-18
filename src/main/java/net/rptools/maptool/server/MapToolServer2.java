3a20b0a279c1c806f95a7f36371559fcc4af41ed
Kenneth VanderLinde <kwvanderlinde@gmail.com>
2024-04-18 15:21:10 -0700

    Move callbacks out of MapToolServer2 constructor and into dedicated nested classes

--- src/main/java/net/rptools/maptool/server/MapToolServer2.java
+++ src/main/java/net/rptools/maptool/server/MapToolServer2.java
@@ -60,73 +60,7 @@ public class MapToolServer2 {
     this.config = config;
     this.policy = policy;
     this.playerDatabase = playerDb;
-
-    this.handshakeObserver =
-        new Handshake2.Observer() {
-          @Override
-          public void onCompleted(Handshake2 handshake) {
-            handshake.removeObserver(this);
-            final var connection = handshake.getConnection();
-
-            // TODO AbstractServer then released the handshake here. Unimportant since we do not
-            //  maintain a set of active handshakes, but if we did we should clear it out here.
-
-            if (!handshake.isSuccessful()) {
-              log.error(
-                  "Handshake for connection {} failed with message: {}",
-                  connection.getId(),
-                  handshake.getErrorMessage(),
-                  handshake.getException());
-              connection.close();
-              return;
-            }
-
-            // (1) & (3)
-            // TODO Watch for an error here. Would indicate someone else snuck in with the same ID.
-            server.addConnection(connection);
-
-            // TODO AbstractServer.onCompleted() took the opportunity to reapClients() here. Let's
-            //  add that to the decomposed server as well, through the message pump though.
-
-            // TODO MapToolServer.configureClientConnection() would have added it to assetManagerMap
-
-            final Player connectedPlayer = ((ServerHandshake2) handshake).getPlayer();
-
-            // Send the connected player to each client.
-            // Original in MapToolServer connection does this in the reverse order, but I like this.
-            server.broadcast(
-                null,
-                Message.newBuilder()
-                    .setPlayerConnectedMsg(
-                        PlayerConnectedMsg.newBuilder()
-                            .setPlayer(connectedPlayer.getTransferablePlayer().toDto()))
-                    .build()
-                    .toByteArray());
-            for (Player player : playersByConnectionId.values()) {
-              connection.sendMessage(
-                  null,
-                  Message.newBuilder()
-                      .setPlayerConnectedMsg(
-                          // TODO Why do we not send the transferable player as above? Actually it
-                          //  does not matter since the DTO only contains transferrable properties.
-                          PlayerConnectedMsg.newBuilder().setPlayer(player.toDto()))
-                      .build()
-                      .toByteArray());
-            }
-
-            // Send the campaign to the new client.
-            connection.sendMessage(
-                null,
-                Message.newBuilder()
-                    .setSetCampaignMsg(SetCampaignMsg.newBuilder().setCampaign(campaign.toDto()))
-                    .build()
-                    .toByteArray());
-
-            // Important that we do this here, otherwise the loop above will send a connection
-            // messages to the new client for itself.
-            playersByConnectionId.put(connection.getId(), connectedPlayer);
-          }
-        };
+    this.handshakeObserver = new HandshakeObserver();
 
     // TODO Obviously this casting will not work. We will have to change ServerMessageHandler
     //  to accept a MapToolServer2. There's not much about it, mostly the message handler just
@@ -138,52 +72,7 @@ public class MapToolServer2 {
     //  be feasible to create at the call site and injected instead of a hard dependency here).
     this.connectionHandler = new SocketConnectionHandler();
 
-    this.connectionHandler.addListener(
-        new ConnectionHandler.Listener() {
-          @Override
-          public void onConnected(@NotNull Connection connection) {
-            executor.execute(
-                () -> {
-                  /*
-                   * TODO I feel like I ought to maintain a set of current handshakes, to prevent
-                   *  two from running for the same client. But that comes with two downsides:
-                   *  1. Both could be legit, so why prefer the one that got in a few ms earlier?
-                   *  2. it's extra state to juggle, so we need a really good reason for it.
-                   */
-
-                  // Start the handshake.
-                  final var handshake =
-                      new ServerHandshake2(connection, playerDatabase, config.getUseEasyConnect());
-                  handshake.addObserver(handshakeObserver);
-                  handshake.startHandshake();
-
-                  connection.start();
-                });
-          }
-
-          @Override
-          public void onConnectionClosed(@NotNull String connectionId) {
-            // Cancel any pending handshake and remove from the server.
-          }
-
-          @Override
-          public void onConnectionLost(@NotNull String connectionId, @NotNull String reason) {
-            // Cancel any pending handshake and remove from the server. Possibly close it again?
-          }
-        });
-
-    // TODO Hook up observers thusly:
-    //  1. When a client connects, begin the handshake.
-    //  2. When the handshake completes, install the connection to `this.server`. When done,
-    //     send the handshake acknowledgement.
-    //  3. Only report activity for connections with completed handshakes.
-    //  4. When a message is received from a connection, send it to the message handler.
-
-    // TODO Modify servers so they don't need a campaign of their own. Leave that entirely to
-    //  clients.
-    //  Oh... I can't actually do that. It's true that the server itself doesn't really need a
-    //  campaign, however when a new client connections the server does need to be able to send
-    //  the campaign over.
+    this.connectionHandler.addListener(new ConnectionHandlerListener());
   }
 
   public @Nonnull Campaign getCampaign() {
@@ -200,5 +89,104 @@ public class MapToolServer2 {
 
   public void updateServerPolicy(@Nonnull ServerPolicy policy) {
     this.policy = policy;
+  }
+
+  private final class HandshakeObserver implements Handshake2.Observer {
+    @Override
+    public void onCompleted(Handshake2 handshake) {
+      handshake.removeObserver(this);
+      final var connection = handshake.getConnection();
+
+      // TODO AbstractServer then released the handshake here. Unimportant since we do not
+      //  maintain a set of active handshakes, but if we did we should clear it out here.
+
+      if (!handshake.isSuccessful()) {
+        log.error(
+            "Handshake for connection {} failed with message: {}",
+            connection.getId(),
+            handshake.getErrorMessage(),
+            handshake.getException());
+        connection.close();
+        return;
+      }
+
+      // (1) & (3)
+      // TODO Watch for an error here. Would indicate someone else snuck in with the same ID.
+      server.addConnection(connection);
+
+      // TODO AbstractServer.onCompleted() took the opportunity to reapClients() here. Let's
+      //  add that to the decomposed server as well, through the message pump though.
+
+      // TODO MapToolServer.configureClientConnection() would have added it to assetManagerMap
+
+      final Player connectedPlayer = ((ServerHandshake2) handshake).getPlayer();
+
+      // Send the connected player to each client.
+      // Original in MapToolServer connection does this in the reverse order, but I like this.
+      server.broadcast(
+          null,
+          Message.newBuilder()
+              .setPlayerConnectedMsg(
+                  PlayerConnectedMsg.newBuilder()
+                      .setPlayer(connectedPlayer.getTransferablePlayer().toDto()))
+              .build()
+              .toByteArray());
+      for (Player player : playersByConnectionId.values()) {
+        connection.sendMessage(
+            null,
+            Message.newBuilder()
+                .setPlayerConnectedMsg(
+                    // TODO Why do we not send the transferable player as above? Actually it
+                    //  does not matter since the DTO only contains transferrable properties.
+                    PlayerConnectedMsg.newBuilder().setPlayer(player.toDto()))
+                .build()
+                .toByteArray());
+      }
+
+      // Send the campaign to the new client.
+      connection.sendMessage(
+          null,
+          Message.newBuilder()
+              .setSetCampaignMsg(SetCampaignMsg.newBuilder().setCampaign(campaign.toDto()))
+              .build()
+              .toByteArray());
+
+      // Important that we do this here, otherwise the loop above will send a connection
+      // messages to the new client for itself.
+      playersByConnectionId.put(connection.getId(), connectedPlayer);
+    }
+  }
+
+  private final class ConnectionHandlerListener implements ConnectionHandler.Listener {
+    @Override
+    public void onConnected(@NotNull Connection connection) {
+      executor.execute(
+          () -> {
+            /*
+             * TODO I feel like I ought to maintain a set of current handshakes, to prevent
+             *  two from running for the same client. But that comes with two downsides:
+             *  1. Both could be legit, so why prefer the one that got in a few ms earlier?
+             *  2. it's extra state to juggle, so we need a really good reason for it.
+             */
+
+            // Start the handshake.
+            final var handshake =
+                new ServerHandshake2(connection, playerDatabase, config.getUseEasyConnect());
+            handshake.addObserver(handshakeObserver);
+            handshake.startHandshake();
+
+            connection.start();
+          });
+    }
+
+    @Override
+    public void onConnectionClosed(@NotNull String connectionId) {
+      // Cancel any pending handshake and remove from the server.
+    }
+
+    @Override
+    public void onConnectionLost(@NotNull String connectionId, @NotNull String reason) {
+      // Cancel any pending handshake and remove from the server. Possibly close it again?
+    }
   }
 }
