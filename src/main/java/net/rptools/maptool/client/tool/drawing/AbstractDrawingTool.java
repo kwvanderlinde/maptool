a93563d5c90a8e1ee8c35f94c52f1ba9506c0d3c
Kenneth VanderLinde <kwvanderlinde@gmail.com>
2024-10-15 19:51:21 -0700

    [topology] Remove AbstractDrawingTool.paintTopologyOverlay() overloads as they are no longer used

--- src/main/java/net/rptools/maptool/client/tool/drawing/AbstractDrawingTool.java
+++ src/main/java/net/rptools/maptool/client/tool/drawing/AbstractDrawingTool.java
@@ -25,7 +25,6 @@ import java.awt.geom.AffineTransform;
 import java.awt.geom.Area;
 import java.awt.geom.Path2D;
 import java.util.List;
-import net.rptools.maptool.client.AppStyle;
 import net.rptools.maptool.client.MapTool;
 import net.rptools.maptool.client.MapToolUtil;
 import net.rptools.maptool.client.ScreenPoint;
@@ -39,9 +38,7 @@ import net.rptools.maptool.model.Zone;
 import net.rptools.maptool.model.Zone.Layer;
 import net.rptools.maptool.model.ZonePoint;
 import net.rptools.maptool.model.drawing.Drawable;
-import net.rptools.maptool.model.drawing.DrawableColorPaint;
 import net.rptools.maptool.model.drawing.Pen;
-import net.rptools.maptool.model.drawing.ShapeDrawable;
 
 // TODO This doc comment is insane and wrong.
 // TODO Get your act together. This makes no sense as a base class for drawings, templates,
@@ -235,75 +232,6 @@ public abstract class AbstractDrawingTool extends DefaultTool implements ZoneOve
 
   @Override
   public abstract void paintOverlay(ZoneRenderer renderer, Graphics2D g);
-
-  protected void paintTopologyOverlay(Graphics2D g, Shape shape, int penMode) {
-    ShapeDrawable drawable = null;
-
-    if (shape != null) {
-      drawable = new ShapeDrawable(shape, false);
-    }
-
-    paintTopologyOverlay(g, drawable, penMode);
-  }
-
-  protected void paintTopologyOverlay(Graphics2D g, Drawable drawable, int penMode) {
-    if (MapTool.getPlayer().isGM()) {
-      Zone zone = renderer.getZone();
-
-      Graphics2D g2 = (Graphics2D) g.create();
-      g2.translate(renderer.getViewOffsetX(), renderer.getViewOffsetY());
-      g2.scale(renderer.getScale(), renderer.getScale());
-
-      g2.setColor(AppStyle.tokenMblColor);
-      g2.fill(getTokenTopology(Zone.TopologyType.MBL));
-      g2.setColor(AppStyle.tokenTopologyColor);
-      g2.fill(getTokenTopology(Zone.TopologyType.WALL_VBL));
-      g2.setColor(AppStyle.tokenHillVblColor);
-      g2.fill(getTokenTopology(Zone.TopologyType.HILL_VBL));
-      g2.setColor(AppStyle.tokenPitVblColor);
-      g2.fill(getTokenTopology(Zone.TopologyType.PIT_VBL));
-      g2.setColor(AppStyle.tokenCoverVblColor);
-      g2.fill(getTokenTopology(Zone.TopologyType.COVER_VBL));
-
-      g2.setColor(AppStyle.topologyTerrainColor);
-      g2.fill(zone.getTopology(Zone.TopologyType.MBL));
-
-      g2.setColor(AppStyle.topologyColor);
-      g2.fill(zone.getTopology(Zone.TopologyType.WALL_VBL));
-
-      g2.setColor(AppStyle.hillVblColor);
-      g2.fill(zone.getTopology(Zone.TopologyType.HILL_VBL));
-
-      g2.setColor(AppStyle.pitVblColor);
-      g2.fill(zone.getTopology(Zone.TopologyType.PIT_VBL));
-
-      g2.setColor(AppStyle.coverVblColor);
-      g2.fill(zone.getTopology(Zone.TopologyType.COVER_VBL));
-
-      g2.dispose();
-    }
-
-    if (drawable != null) {
-      Pen pen = new Pen();
-      pen.setEraser(getPen().isEraser());
-      pen.setOpacity(AppStyle.topologyRemoveColor.getAlpha() / 255.0f);
-      pen.setBackgroundMode(penMode);
-
-      if (penMode == Pen.MODE_TRANSPARENT) {
-        pen.setThickness(3.0f);
-      }
-
-      if (pen.isEraser()) {
-        pen.setEraser(false);
-      }
-      if (isEraser()) {
-        pen.setBackgroundPaint(new DrawableColorPaint(AppStyle.topologyRemoveColor));
-      } else {
-        pen.setBackgroundPaint(new DrawableColorPaint(AppStyle.topologyAddColor));
-      }
-      paintTransformed(g, renderer, drawable, pen);
-    }
-  }
 
   /**
    * Render a drawable on a zone. This method consolidates all of the calls to the server in one
