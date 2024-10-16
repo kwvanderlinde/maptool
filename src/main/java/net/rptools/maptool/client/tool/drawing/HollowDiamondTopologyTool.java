5bc0fc03b258275de337c860bf63565e350572bc
Kenneth VanderLinde <kwvanderlinde@gmail.com>
2024-10-15 20:37:20 -0700

    [topology] Fix bug where hollow diamond topology tool uses color picker thickness; also means it does not need a pen anymore :thumbsup:

--- src/main/java/net/rptools/maptool/client/tool/drawing/HollowDiamondTopologyTool.java
+++ src/main/java/net/rptools/maptool/client/tool/drawing/HollowDiamondTopologyTool.java
@@ -14,11 +14,11 @@
  */
 package net.rptools.maptool.client.tool.drawing;
 
-import java.awt.Graphics2D;
-import java.awt.Shape;
+import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 import java.awt.geom.Area;
+import java.awt.geom.Path2D;
 import javax.swing.SwingUtilities;
 import net.rptools.maptool.client.MapTool;
 import net.rptools.maptool.client.ui.zone.renderer.ZoneRenderer;
@@ -27,6 +27,9 @@ import net.rptools.maptool.model.ZonePoint;
 public class HollowDiamondTopologyTool extends AbstractTopologyDrawingTool
     implements MouseMotionListener {
   private static final long serialVersionUID = 7227397975734203085L;
+
+  // TODO I would like line thickness to be decided by AbstractTopologyDrawingTool.
+  private final float thickness = 2.f;
   protected Shape diamond;
   protected ZonePoint originPoint;
 
@@ -58,11 +61,9 @@ public class HollowDiamondTopologyTool extends AbstractTopologyDrawingTool
     if (SwingUtilities.isLeftMouseButton(e)) {
       if (diamond == null) {
         originPoint = zp;
-        // TODO Bug: getPen() uses the color picker thickness. We should not.
-        diamond = createHollowDiamond(originPoint, originPoint, getPen());
+        diamond = createHollowDiamond(originPoint, originPoint);
       } else {
-        // TODO Bug: getPen() uses the color picker thickness. We should not.
-        diamond = createHollowDiamond(originPoint, zp, getPen());
+        diamond = createHollowDiamond(originPoint, zp);
         // diamond = createDiamond(originPoint, zp);
 
         if (diamond.getBounds().width == 0 || diamond.getBounds().height == 0) {
@@ -119,5 +120,33 @@ public class HollowDiamondTopologyTool extends AbstractTopologyDrawingTool
     } else {
       super.resetTool();
     }
+  }
+
+  protected Shape createHollowDiamond(ZonePoint originPoint, ZonePoint newPoint) {
+    int ox = originPoint.x;
+    int oy = originPoint.y;
+    int nx = newPoint.x;
+    int ny = newPoint.y;
+    int x1 = ox - (ny - oy) + ((nx - ox) / 2);
+    int y1 = ((oy + ny) / 2) - ((nx - ox) / 4);
+    int x2 = ox + (ny - oy) + ((nx - ox) / 2);
+    int y2 = ((oy + ny) / 2) + ((nx - ox) / 4);
+    int[] x = {originPoint.x, x1, nx, x2, originPoint.x};
+    int[] y = {originPoint.y, y1, ny, y2, originPoint.y};
+
+    BasicStroke stroke = new BasicStroke(thickness, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER);
+
+    Path2D path = new Path2D.Double();
+
+    for (int l = 0; l < 5; l++) {
+      if (path.getCurrentPoint() == null) {
+        path.moveTo(x[l], y[l]);
+      } else {
+        path.lineTo(x[l], y[l]);
+      }
+    }
+
+    Area area = new Area(stroke.createStrokedShape(path));
+    return area;
   }
 }
