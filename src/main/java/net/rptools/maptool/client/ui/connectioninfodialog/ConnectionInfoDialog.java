0443325377e62b31f5e05eb9b3612d0fe2e65db4
Richard Maw <richard.maw@gmail.com>
2024-12-06 21:44:14 +0000

    Sort addresses by reachability and pick the most
    Rather than picking the first non-loopback, non-link-local address for
    each protocol, get all the non-loopback addresses,
    sort them by reachability, including whether they are routable to
    www.rptools.net, and pick the first.
    
    This means if you have no internet-routable addresses
    you can still use site-local ones,
    and if you have no site-local ones you can use link-local.

--- src/main/java/net/rptools/maptool/client/ui/connectioninfodialog/ConnectionInfoDialog.java
+++ src/main/java/net/rptools/maptool/client/ui/connectioninfodialog/ConnectionInfoDialog.java
@@ -16,14 +16,21 @@ package net.rptools.maptool.client.ui.connectioninfodialog;
 
 import java.awt.GridLayout;
 import java.io.IOException;
+import java.net.Inet4Address;
 import java.net.Inet6Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.Socket;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Enumeration;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ForkJoinPool;
+import java.util.stream.Collectors;
+import javax.annotation.Nonnull;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComponent;
@@ -44,59 +51,148 @@ public class ConnectionInfoDialog extends JDialog {
 
   private static final Logger log = LogManager.getLogger(ConnectionInfoDialog.class);
 
+  record LocalAddresses(@Nonnull List<Inet4Address> ipv4, @Nonnull List<Inet6Address> ipv6) {}
+
+  record AddressInfo<T extends InetAddress>(
+      @Nonnull T address, boolean isRoutable, boolean isLinkLocal) {}
+
   /**
-   * Get the IP-Address of the active NetworkInterface
+   * Get properties of the address necessary to sort in reachability order.
    *
-   * @param v6 Should the IPv6 Address be returnes (true) or the IPv4 Address (false)
+   * <p>This adds to the list directly if this requires no network requests or adds a task to do so
+   * to the tasks list.
+   *
+   * <p>This checks routability by connecting from this address to the rptools one using SSL's port
+   * 443 rather than IGMP (ping) or TCP port 7 so strict firewalls won't cause false negatives.
+   *
+   * @param address The address to get info for
+   * @param rptools The rptools address to check routability to
+   * @param infos Output list to add info to
+   * @param tasks Output list of tasks that should be awaited for results
    */
-  private static String getIPAddress(boolean v6) throws SocketException {
-    // Get the appropriate IPv4/6 address for future reachability checks
-    InetAddress rptools = null;
-    try {
-      for (InetAddress addr : InetAddress.getAllByName("www.rptools.net")) {
-        if (v6 == (addr instanceof Inet6Address)) {
-          rptools = addr;
-          break;
-        }
-      }
-    } catch (UnknownHostException ignore) {
+  private static <T extends InetAddress> void getInfo(
+      @Nonnull T address,
+      @Nonnull T rptools,
+      @Nonnull List<AddressInfo<T>> infos,
+      @Nonnull List<Callable<Void>> tasks) {
+    if (rptools == null) {
+      infos.add(new AddressInfo(address, true /*isRoutable*/, address.isLinkLocalAddress()));
+      return;
     }
-    // We might find we didn't resolve an address for our address family
-    // but we can fall back to skipping the reachability check
-    // since rptools.net not having an address doesn't imply we're unreachable.
 
-    Enumeration<NetworkInterface> netInts = NetworkInterface.getNetworkInterfaces();
-    for (NetworkInterface netInt : Collections.list(netInts)) {
-      if (!netInt.isUp() || netInt.isLoopback()) {
-        continue;
-      }
-
-      for (InetAddress inetAddress : Collections.list(netInt.getInetAddresses())) {
-
-        if (inetAddress.isLoopbackAddress()
-            || inetAddress.isLinkLocalAddress()
-            || inetAddress.isMulticastAddress()) {
-          continue;
-        }
-
-        if (v6 != (inetAddress instanceof Inet6Address)) {
-          continue;
-        }
-
-        // Check that rptools is reachable from this address
-        // as a proxy that this address is reachable by others
-        // by connecting to rptools' SSL port from this local address.
-        if (rptools != null) {
-          try (var s = new Socket(rptools, 443, inetAddress, 0)) {
+    tasks.add(
+        () -> {
+          boolean isRoutable = true;
+          try (var s = new Socket(rptools, 443, address, 0)) {
           } catch (IOException | SecurityException | IllegalArgumentException e) {
-            continue;
+            isRoutable = false;
           }
-        }
+          infos.add(new AddressInfo(address, isRoutable, address.isLinkLocalAddress()));
+          return null;
+        });
+  }
 
-        return inetAddress.getHostAddress();
-      }
-    }
-    return null;
+  /**
+   * Sort the address information into order of most reachable first.
+   *
+   * <p>Routable addresses are more reachable than non-routable then non-link-local addresses are
+   * more reachable than link-local ones.
+   */
+  private static <T extends InetAddress> void sortInfos(@Nonnull List<AddressInfo<T>> infos) {
+    infos.sort(
+        (o1, o2) -> {
+          if (o1.isRoutable() != o2.isRoutable()) {
+            return o1.isRoutable() ? -1 : 1;
+          }
+          return (o1.isLinkLocal() ? 1 : 0) - (o2.isLinkLocal() ? 1 : 0);
+        });
+  }
+
+  /**
+   * Asynchronously get all local addresses in order of most reachable first.
+   *
+   * <p>This checks routability to www.rptools.net using the SSL port 443 from each address in
+   * parallel and uses this and if it's link-local to sort the addresses in order of most reachable
+   * first.
+   */
+  @Nonnull
+  private CompletableFuture<LocalAddresses> getLocalAddresses() {
+    return CompletableFuture.supplyAsync(
+        () -> {
+          // Get the appropriate IPv4/6 address for future reachability checks
+          Inet4Address rptools4 = null;
+          Inet6Address rptools6 = null;
+          try {
+            for (InetAddress addr : InetAddress.getAllByName("www.rptools.net")) {
+              switch (addr) {
+                case Inet4Address a -> {
+                  if (rptools4 != null) {
+                    continue;
+                  }
+                  rptools4 = a;
+                }
+                case Inet6Address a -> {
+                  if (rptools6 != null) {
+                    continue;
+                  }
+                  rptools6 = a;
+                }
+                default -> {
+                  continue;
+                }
+              }
+
+              if (rptools4 != null && rptools6 != null) {
+                break;
+              }
+            }
+          } catch (UnknownHostException ignore) {
+          }
+          // We might find we didn't resolve an address for our address family
+          // but we can fall back to skipping the reachability check
+          // since rptools.net not having an address doesn't imply we're unreachable.
+
+          // Get all addresses, concurrently checking routability to rptools
+          // and partition them into v4 and v6.
+          List<AddressInfo<Inet4Address>> v4Infos = new ArrayList();
+          List<AddressInfo<Inet6Address>> v6Infos = new ArrayList();
+          List<Callable<Void>> tasks = new ArrayList();
+          List<NetworkInterface> netInts;
+          try {
+            netInts = Collections.list(NetworkInterface.getNetworkInterfaces());
+          } catch (SocketException e) {
+            netInts = List.of();
+          }
+          for (NetworkInterface netInt : netInts) {
+            try {
+              if (!netInt.isUp() || netInt.isLoopback()) {
+                continue;
+              }
+            } catch (SocketException e) {
+              continue;
+            }
+
+            for (InetAddress inetAddress : Collections.list(netInt.getInetAddresses())) {
+              if (inetAddress.isLoopbackAddress() || inetAddress.isMulticastAddress()) {
+                continue;
+              }
+
+              switch (inetAddress) {
+                case Inet4Address a -> getInfo(a, rptools4, v4Infos, tasks);
+                case Inet6Address a -> getInfo(a, rptools6, v6Infos, tasks);
+                default -> {}
+              }
+            }
+          }
+          ForkJoinPool.commonPool().invokeAll(tasks);
+
+          // Return addresses most reachable first
+          sortInfos(v4Infos);
+          sortInfos(v6Infos);
+          return new LocalAddresses(
+              v4Infos.stream().map(x -> x.address()).collect(Collectors.toList()),
+              v6Infos.stream().map(x -> x.address()).collect(Collectors.toList()));
+        });
   }
 
   /**
@@ -122,28 +218,25 @@ public class ConnectionInfoDialog extends JDialog {
       name = "---";
     }
 
-    String localv4Address = "Unknown";
-    try {
-      localv4Address = getIPAddress(false);
-    } catch (IOException e) { // UnknownHost | Socket
-      log.warn("Can't resolve our own IPv4 address!?", e);
-    }
-
-    String localv6Address = "Unknown";
-    try {
-      localv6Address = getIPAddress(true);
-    } catch (IOException e) { // UnknownHost | Socket
-      log.warn("Can't resolve our own IPv6 address!?", e);
-    }
-
     int port = server.getPort();
     String portString = port < 0 ? "---" : Integer.toString(port);
 
     nameLabel.setText(name);
-    localv4AddressLabel.setText(localv4Address);
-    localv6AddressLabel.setText(localv6Address);
+    localv4AddressLabel.setText("Unknown");
+    localv6AddressLabel.setText("Unknown");
     externalAddressLabel.setText(I18N.getText("ConnectionInfoDialog.discovering"));
     portLabel.setText(portString);
+
+    getLocalAddresses()
+        .thenAccept(
+            localAddresses -> {
+              if (!localAddresses.ipv4().isEmpty()) {
+                localv4AddressLabel.setText(NetUtil.formatAddress(localAddresses.ipv4().get(0)));
+              }
+              if (!localAddresses.ipv6().isEmpty()) {
+                localv6AddressLabel.setText(NetUtil.formatAddress(localAddresses.ipv6().get(0)));
+              }
+            });
 
     JButton okButton = (JButton) panel.getButton("okButton");
     bindOKButtonActions(okButton);
