f879ccd68ddca8e25d0943bce5bc122f4a364802
Kenneth VanderLinde <kwvanderlinde@gmail.com>
2025-10-27 16:28:10 -0700

    TEMP Disable continuous rendering for ZoneCompositor

--- src/main/java/net/rptools/maptool/client/ui/zone/renderer/ZoneCompositor.java
+++ src/main/java/net/rptools/maptool/client/ui/zone/renderer/ZoneCompositor.java
@@ -399,8 +399,6 @@ public class ZoneCompositor {
       }
     }
 
-    // Continuous rendering
-    viewModel.repaintNeeded();
     return new InstructionSet(viewport, ImmutableList.copyOf(instructions), clips);
   }
 
