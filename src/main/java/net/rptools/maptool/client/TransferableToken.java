dd9fcfaca639814323b1e8ad28b0e05f0491e53c
Kenneth VanderLinde <kwvanderlinde@gmail.com>
2026-07-15 08:15:34 -0700

    Revert "BROKEN Trying to proper bubble assets in drag-n-drop"
    This reverts commit bbd33eb2f0e3244b7cf93a8fd6b9bff5f9f82a9c.

--- src/main/java/net/rptools/maptool/client/TransferableToken.java
+++ src/main/java/net/rptools/maptool/client/TransferableToken.java
@@ -21,7 +21,6 @@ import net.rptools.maptool.model.Asset;
 import net.rptools.maptool.model.Token;
 
 public class TransferableToken implements Transferable {
-  // TODO Any consumers of this data flavor need to consume assets as well.
   public static final DataFlavor dataFlavor = new DataFlavor(Token.class, "Token");
 
   private Token token;
