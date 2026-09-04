e856785d58c7e1187634465bdbc02d6b992cff7c
Kenneth VanderLinde <kwvanderlinde@gmail.com>
2026-09-04 12:06:57 -0700

    Remove unused Zone#isTokenFootprintVisible(Token)

--- src/main/java/net/rptools/maptool/model/Zone.java
+++ src/main/java/net/rptools/maptool/model/Zone.java
@@ -901,43 +901,6 @@ public class Zone {
     return combined.intersects(tokenSize);
   }
 
-  public boolean isTokenFootprintVisible(Token token) {
-    if (token == null) {
-      return false;
-    }
-    // Base case, nothing is visible
-    if (!token.isVisible()) {
-      return false;
-    }
-    // Base case, everything is visible
-    if (!hasFog()) {
-      return true;
-    }
-    if (token.isVisibleOnlyToOwner() && !AppUtil.playerOwns(token)) {
-      return false;
-    }
-    // Token is visible, and there is fog
-    Rectangle tokenSize = token.getFootprintBounds(this);
-    Area tokenFootprint = getGrid().getTokenCellArea(tokenSize);
-    Area combined = new Area(exposedArea);
-    PlayerView view = MapTool.getFrame().getZoneRenderer(this).getPlayerView();
-    if (MapTool.getServerPolicy().isUseIndividualFOW() && getVisionType() != VisionType.OFF) {
-      if (view.isUsingTokenView()) {
-        // Should this use FindTokenFunctions.OwnedFilter and zone.getTokenList()?
-        for (Token tok : view.getTokens()) {
-          if (!AppUtil.playerOwns(tok)) {
-            continue;
-          }
-          if (exposedAreaMeta.containsKey(tok.getExposedAreaGUID())) {
-            combined.add(exposedAreaMeta.get(tok.getExposedAreaGUID()).getExposedAreaHistory());
-          }
-        }
-      }
-    }
-    combined.intersect(tokenFootprint);
-    return !combined.isEmpty();
-  }
-
   public WallTopology getWalls() {
     return walls;
   }
